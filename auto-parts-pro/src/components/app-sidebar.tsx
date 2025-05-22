99959c6963cd4fdce8574e1d9063f619d6462e93
Iván Martínez <ivanmartinez33@outlook.com>
2025-05-22 00:47:02 -0600

    basic sidebar

---
+++ auto-parts-pro/src/components/app-sidebar.tsx
@@ -0,0 +1,83 @@
+import {
+  BoltIcon,
+  BookUserIcon,
+  CarFrontIcon,
+  CarIcon,
+  WrenchIcon,
+} from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+// Menu items.
+const items = [
+  {
+    title: "Repair Orders",
+    url: "/repair-orders",
+    icon: WrenchIcon,
+  },
+  {
+    title: "Customers",
+    url: "/customers",
+    icon: BookUserIcon,
+  },
+  {
+    title: "Vehicles",
+    url: "/vehicles",
+    icon: CarIcon,
+  },
+  {
+    title: "Parts",
+    url: "/parts",
+    icon: BoltIcon,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
+            >
+              <a href="#">
+                <CarFrontIcon className="h-5 w-5" />
+                <span className="text-base font-semibold">Auto Parts Pro</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          {/* <SidebarGroupLabel>Aplicación</SidebarGroupLabel> */}
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
